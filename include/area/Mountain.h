d756175dfafc419485b01ceb361fc29d9cfb1bfc
Kai Tamkun <ktamkun@ucsc.edu>
2021-03-09 01:38:27 -0800

    Mountains slowly regenerate iron ore and coal.

--- include/area/Mountain.h
+++ include/area/Mountain.h
@@ -4,6 +4,9 @@
 
 class MountainArea: public Area {
 	public:
+		constexpr static double COAL_MAX = 100.;
+		constexpr static double IRON_MAX = 100.;
+
 		MountainArea(Region *, size_t = 0);
 		Type getType() const override { return Type::Mountain; }
 		std::string description() const override;
--- source/area/Mountain.cpp
+++ source/area/Mountain.cpp
@@ -3,12 +3,17 @@
 #include "Game.h"
 
 MountainArea::MountainArea(Region *region, size_t size): Area(region, size) {
-	resources.emplace("ironOre", static_cast<double>(size));
-	resources.emplace("coal", static_cast<double>(size));
+	resources.emplace("ironOre", static_cast<double>(size) * IRON_MAX);
+	resources.emplace("coal", static_cast<double>(size) * COAL_MAX);
 }
 
 std::string MountainArea::description() const {
 	return "Resource-rich mountainous terrain.";
 }
 
-void MountainArea::tick() {}
+void MountainArea::tick() {
+	if ((resources["coal"] += static_cast<double>(size) * 0.01) > static_cast<double>(size) * COAL_MAX)
+		 resources["coal"]  = static_cast<double>(size) * COAL_MAX;
+	if ((resources["ironOre"] += static_cast<double>(size) * 0.01) > static_cast<double>(size) * IRON_MAX)
+		 resources["ironOre"]  = static_cast<double>(size) * IRON_MAX;
+}
