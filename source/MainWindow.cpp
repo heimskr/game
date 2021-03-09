1c7c57465e99b921ab0265e2688f464120319457
Kai Tamkun <ktamkun@ucsc.edu>
2021-03-09 04:59:33 -0800

    Improve Travel tab

--- source/MainWindow.cpp
+++ source/MainWindow.cpp
@@ -47,6 +47,9 @@ void MainWindow::render(bool *open) {
 				ImGui::PushFont(UI::getFont("Nintendo Standard Big"));
 				ImGui::Text("%s", region->name.c_str());
 				ImGui::PopFont();
+				ImGui::SameLine();
+				ImGui::Dummy({6.f, 0.f});
+				ImGui::SameLine();
 				if (ImGui::Button("Rename")) {
 					Keyboard::openForText([&](std::string new_name) {
 						if (new_name.empty() || new_name == region->name) {
@@ -104,36 +107,43 @@ void MainWindow::render(bool *open) {
 				ImGui::Text("Travel is not possible within the void.");
 			} else {
 				ImGui::Text("Your position is (%ld, %ld).", region->position.first, region->position.second);
-				ImGui::Columns(2, nullptr, false);
-
-				static int set_width = 0;
-				if (set_width == 0) {
-					ImGui::SetColumnWidth(-1, 110.f);
-					++set_width;
-				}
-
-				for (int i = 0; i < 4; ++i) {
-					Direction direction = static_cast<Direction>(i);
-					const bool exists = region->hasNeighbor(direction);
-					if (ImGui::Button(toString(direction), ImVec2(100.f, 0.f))) {
-						const auto &offset = getOffset(direction);
-						Region::Position pos(context->position.first + offset.first, context->position.second + offset.second);
-						if (exists) {
-							context->position = pos;
-						} else {
-							std::unique_ptr<Region> new_region = Region::generate(*context.game, pos);
-							*context.game += std::move(new_region);
+				if (ImGui::BeginTable("Map", 3)) {
+					constexpr float COLUMN_WIDTH = 200.f;
+					ImGui::TableSetupColumn("", ImGuiTableColumnFlags_WidthFixed, COLUMN_WIDTH);
+					ImGui::TableSetupColumn("", ImGuiTableColumnFlags_WidthFixed, COLUMN_WIDTH);
+					ImGui::TableSetupColumn("", ImGuiTableColumnFlags_WidthFixed, COLUMN_WIDTH);
+					for (int row = 0; row < 3; ++row) {
+						ImGui::TableNextRow();
+						for (int col = 0; col < 3; ++col) {
+							ImGui::TableSetColumnIndex(col);
+							const Region::Position pos(region->position.first + col - 1, region->position.second + row - 1);
+							std::string label = "?";
+							bool exists = false;
+							if (context->regions.count(pos) != 0) {
+								label = context->regions.at(pos)->name;
+								exists = true;
+							}
+							label += "##" + std::to_string(col) + "_" + std::to_string(row);
+							const bool disabled = row == 1 && col == 1;
+							if (disabled) {
+								ImGui::PushItemFlag(ImGuiItemFlags_Disabled, true);
+								ImGui::PushStyleVar(ImGuiStyleVar_Alpha, ImGui::GetStyle().Alpha * 0.75f);
+							}
+							if (ImGui::Button(label.c_str(), {COLUMN_WIDTH, 100.f})) {
+								if (exists)
+									context->position = pos;
+								else
+									*context.game += Region::generate(*context.game, pos);
+							}
+							if (disabled) {
+								ImGui::PopItemFlag();
+								ImGui::PopStyleVar();
+							}
+							ImGui::TableNextColumn();
 						}
 					}
-					ImGui::NextColumn();
-					if (!exists)
-						ImGui::Text("?");
-					else if (Region *neighbor = region->getNeighbor(direction))
-						ImGui::Text("%s", neighbor->name.c_str());
-					ImGui::NextColumn();
+					ImGui::EndTable();
 				}
-
-				ImGui::Columns(1);
 			}
 			ImGui::EndTabItem();
 		}
