3737d31d363a48ae06ebf8d983f9cdd31eed4b62
Kai Tamkun <ktamkun@ucsc.edu>
2021-03-08 22:49:54 -0800

    Use a tab bar

--- source/MainWindow.cpp
+++ source/MainWindow.cpp
@@ -30,62 +30,75 @@ void MainWindow(Context &context, bool *open) {
 		}
 	}
 
-	if (!context.game || !context.loaded) {
-		ImGui::Text("No game is loaded.");
-	} else {
-		Region *region = nullptr;
-		try {
-			region = &context->currentRegion();
-		} catch (const std::exception &err) {
-			std::string text = "Error: " + std::string(err.what());
-			ImGui::Text("%s", text.c_str());
-		}
-
-		if (region) {
-			ImGui::PushFont(UI::getFont("Nintendo Standard Big"));
-			ImGui::Text("%s", region->name.c_str());
-			ImGui::PopFont();
-			if (ImGui::Button("Rename")) {
-				Keyboard::openForText([&](std::string new_name) {
-					if (new_name.empty() || new_name == region->name) {
-						context.message = "Name not updated.";
-					} else {
-						context.message = "Renamed " + region->name + " to " + new_name + ".";
-						context->updateName(*region, new_name);
-					}
-				}, "New Region Name", "", 64, region->name);
-			}
-			if (region->areas.empty()) {
-				ImGui::Dummy(ImVec2(20.f, 0.f));
-				ImGui::SameLine();
-				ImGui::Text("Region has no areas.");
+	if (ImGui::BeginTabBar("##tabs", ImGuiTabBarFlags_None)) {
+		if (ImGui::BeginTabItem("Region")) {
+			if (!context.game || !context.loaded) {
+				ImGui::Text("No game is loaded.");
 			} else {
-				for (const auto &[name, area]: region->areas) {
-					ImGui::SetNextItemOpen(true, ImGuiCond_Once);
-					if (ImGui::TreeNode(name.c_str())) {
-						for (const auto &[rname, amount]: area->resources) {
-							ImGui::Dummy(ImVec2(20.f, 0.f));
-							ImGui::SameLine();
-							ImGui::PushID(rname.c_str());
-							if (ImGui::Button("Extract")) {
-								Keyboard::openForDouble([&](double chosen) {
-									if (amount < chosen) {
-										context.message = "Not enough of that resource is available.";
-									} else {
-										context->extractions.emplace_back(area.get(), rname, chosen, context->resources.at(rname).defaultExtractionRate);
-										context.message = "Extracting " + std::to_string(chosen) + " x " + rname + ".";
-									}
-								}, "Amount to Extract");
+				Region *region = nullptr;
+				try {
+					region = &context->currentRegion();
+				} catch (const std::exception &err) {
+					std::string text = "Error: " + std::string(err.what());
+					ImGui::Text("%s", text.c_str());
+				}
+
+				if (region) {
+					ImGui::PushFont(UI::getFont("Nintendo Standard Big"));
+					ImGui::Text("%s", region->name.c_str());
+					ImGui::PopFont();
+					if (ImGui::Button("Rename")) {
+						Keyboard::openForText([&](std::string new_name) {
+							if (new_name.empty() || new_name == region->name) {
+								context.message = "Name not updated.";
+							} else {
+								context.message = "Renamed " + region->name + " to " + new_name + ".";
+								context->updateName(*region, new_name);
 							}
-							ImGui::PopID();
-							ImGui::SameLine();
-							ImGui::Text("%s x %.2f", rname.c_str(), amount);
+						}, "New Region Name", "", 64, region->name);
+					}
+					if (region->areas.empty()) {
+						ImGui::Dummy(ImVec2(20.f, 0.f));
+						ImGui::SameLine();
+						ImGui::Text("Region has no areas.");
+					} else {
+						for (const auto &[name, area]: region->areas) {
+							ImGui::SetNextItemOpen(true, ImGuiCond_Once);
+							if (ImGui::TreeNode(name.c_str())) {
+								for (const auto &[rname, amount]: area->resources) {
+									ImGui::Dummy(ImVec2(20.f, 0.f));
+									ImGui::SameLine();
+									ImGui::PushID(rname.c_str());
+									if (ImGui::Button("Extract")) {
+										Keyboard::openForDouble([&](double chosen) {
+											if (amount < chosen) {
+												context.message = "Not enough of that resource is available.";
+											} else {
+												context->extractions.emplace_back(area.get(), rname, chosen, context->resources.at(rname).defaultExtractionRate);
+												context.message = "Extracting " + std::to_string(chosen) + " x " + rname + ".";
+											}
+										}, "Amount to Extract");
+									}
+									ImGui::PopID();
+									ImGui::SameLine();
+									ImGui::Text("%s x %.2f", rname.c_str(), amount);
+								}
+								ImGui::TreePop();
+							}
 						}
-						ImGui::TreePop();
 					}
 				}
 			}
+
+			ImGui::EndTabItem();
 		}
+
+		if (ImGui::BeginTabItem("Travel")) {
+			ImGui::Text("Travel");
+			ImGui::EndTabItem();
+		}
+
+		ImGui::EndTabBar();
 	}
 
 	ImGui::End();
