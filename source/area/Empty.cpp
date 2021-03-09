061768e1e9dcff4476df1f4ef5bd0fe91ed5b892
Kai Tamkun <ktamkun@ucsc.edu>
2021-03-09 02:09:18 -0800

    Fix empty areas starting out with humans

--- source/area/Empty.cpp
+++ source/area/Empty.cpp
@@ -2,9 +2,7 @@
 #include "Region.h"
 #include "Game.h"
 
-EmptyArea::EmptyArea(Region *region, size_t size): Area(region, size) {
-	resources.emplace("human", static_cast<double>(size / 4));
-}
+EmptyArea::EmptyArea(Region *region, size_t size): Area(region, size) {}
 
 std::string EmptyArea::description() const {
 	return "An empty area.";
