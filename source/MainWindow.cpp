2cf73b0438493b0a7b985497ea2a16f0b2f677e4
Kai Tamkun <ktamkun@ucsc.edu>
2021-03-10 04:34:51 -0800

    Add UI to delete extractions

--- source/MainWindow.cpp
+++ source/MainWindow.cpp
@@ -189,13 +189,21 @@ void MainWindow::render(bool *open) {
 			if (context->extractions.empty()) {
 				ImGui::Text("Nothing is happening.");
 			} else {
-				for (const Extraction &extraction: context->extractions) {
+				s64 i = 0;
+				for (auto iter = context->extractions.begin(), end = context->extractions.end(); iter != end; ++iter) {
+					const Extraction &extraction = *iter;
+					if (ImGui::Button(("x##" + std::to_string(++i)).c_str())) {
+						context.frameActions.push_back([this, iter]() {
+							context->extractions.erase(iter);
+						});
+					}
+					ImGui::SameLine();
 					ImGui::Text("Extracting %s from %s in %s (%.2f left) @ %.2f/s",
 						extraction.resourceName.c_str(), extraction.area->name.c_str(),
 						extraction.area->parent->name.c_str(), extraction.amount, extraction.rate);
 					if (0. < extraction.startAmount) {
-						static char buf[32];
-						snprintf(buf, 32, "%.2f/%.2f", extraction.startAmount - extraction.amount, extraction.startAmount);
+						static char buf[64];
+						snprintf(buf, 64, "%.3f/%.3f", extraction.startAmount - extraction.amount, extraction.startAmount);
 						ImGui::ProgressBar((extraction.startAmount - extraction.amount) / extraction.startAmount, {0, 0}, buf);
 					}
 				}
