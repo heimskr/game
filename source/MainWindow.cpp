c360feb54fbe24df8d26090ac12fedf67d8263c7
Kai Tamkun <ktamkun@ucsc.edu>
2021-03-12 21:53:25 -0800

    Switch Conversion and Market tabs

--- source/MainWindow.cpp
+++ source/MainWindow.cpp
@@ -293,8 +293,45 @@ void MainWindow::render(bool *open) {
 			ImGui::EndTabItem();
 		}
 
-		if (ImGui::BeginTabItem("Market", nullptr, selectedTab == 4? ImGuiTabItemFlags_SetSelected : 0)) {
+		if (ImGui::BeginTabItem("Conversion", nullptr, selectedTab == 4? ImGuiTabItemFlags_SetSelected : 0)) {
 			lastTab = 4;
+			if (!context.game || !context.loaded) {
+				ImGui::Text("You can't convert anything in the void.");
+			} else  {
+				if (ImGui::Button("Add Processor"))
+					context.pickProcessorType([this](Processor::Type type) {
+						if (context->processorCosts.count(type) == 0) {
+							context.showMessage("There is no recipe for " + std::string(Processor::typeName(type))
+								+ " processors.");
+							return;
+						}
+						const Resource::Map &cost = context->processorCosts.at(type);
+						if (!contains(context->inventory, cost)) {
+							context.showMessage("You don't have enough resources to make that.");
+							return;
+						}
+						for (const auto &[name, amount]: cost)
+							context->inventory[name] -= amount;
+						shrink(context->inventory);
+						context->processors.push_back(std::unique_ptr<Processor>(Processor::ofType(*context.game, type)));
+						context.showMessage("Added a new " + std::string(Processor::typeName(type)) + ".");
+					});
+
+				if (context->processors.empty()) {
+					ImGui::Text("You have no processors.");
+				} else {
+					u64 i = 0;
+					for (const std::unique_ptr<Processor> &processor: context->processors) {
+						processor->renderHeader(context, ++i);
+						processor->renderBody(context, ++i);
+					}
+				}
+			}
+			ImGui::EndTabItem();
+		}
+
+		if (ImGui::BeginTabItem("Market", nullptr, selectedTab == 5? ImGuiTabItemFlags_SetSelected : 0)) {
+			lastTab = 5;
 			if (!context.game || !context.loaded) {
 				ImGui::Text("No game is loaded.");
 			} else if (!region) {
@@ -450,43 +487,6 @@ void MainWindow::render(bool *open) {
 
 						ImGui::TableNextColumn();
 						ImGui::EndTable();
-					}
-				}
-			}
-			ImGui::EndTabItem();
-		}
-
-		if (ImGui::BeginTabItem("Conversion", nullptr, selectedTab == 5? ImGuiTabItemFlags_SetSelected : 0)) {
-			lastTab = 5;
-			if (!context.game || !context.loaded) {
-				ImGui::Text("You can't convert anything in the void.");
-			} else  {
-				if (ImGui::Button("Add Processor"))
-					context.pickProcessorType([this](Processor::Type type) {
-						if (context->processorCosts.count(type) == 0) {
-							context.showMessage("There is no recipe for " + std::string(Processor::typeName(type))
-								+ " processors.");
-							return;
-						}
-						const Resource::Map &cost = context->processorCosts.at(type);
-						if (!contains(context->inventory, cost)) {
-							context.showMessage("You don't have enough resources to make that.");
-							return;
-						}
-						for (const auto &[name, amount]: cost)
-							context->inventory[name] -= amount;
-						shrink(context->inventory);
-						context->processors.push_back(std::unique_ptr<Processor>(Processor::ofType(*context.game, type)));
-						context.showMessage("Added a new " + std::string(Processor::typeName(type)) + ".");
-					});
-
-				if (context->processors.empty()) {
-					ImGui::Text("You have no processors.");
-				} else {
-					u64 i = 0;
-					for (const std::unique_ptr<Processor> &processor: context->processors) {
-						processor->renderHeader(context, ++i);
-						processor->renderBody(context, ++i);
 					}
 				}
 			}
