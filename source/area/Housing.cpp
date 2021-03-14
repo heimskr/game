c0f0dde7ace199e2b3a4af7c129922ac87950fbd
Kai Tamkun <ktamkun@ucsc.edu>
2021-03-13 22:36:53 -0800

    Regions consume food and drink

--- source/area/Housing.cpp
+++ source/area/Housing.cpp
@@ -12,7 +12,18 @@ std::string HousingArea::description() const {
 
 void HousingArea::tick(double delta) {
 	double &humans = resources["Human"];
-	if (2.0 <= humans)
-		if ((humans += humans * .002 * delta) > static_cast<double>(size))
+	if (2. <= humans)
+		if ((humans += humans * 0.002 * delta) > static_cast<double>(size))
 			humans = static_cast<double>(size);
+	if (1. <= humans) {
+		Resource::Map non_owned = parent->allNonOwnedResources();
+		std::list<std::pair<const std::string *, double>> subtractions;
+		for (const auto &[name, amount]: non_owned) {
+			const Resource &resource = parent->game->resources.at(name);
+			if (resource.hasType("food") || resource.hasType("drink"))
+				subtractions.emplace_back(&name, std::min(size / 500., amount));
+		}
+		for (const auto &subtraction: subtractions)
+			parent->subtractResourceFromNonOwned(*subtraction.first, subtraction.second);
+	}
 }
